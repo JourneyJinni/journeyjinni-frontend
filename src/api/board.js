7dd7b662b8a3f49b2a2c0099acdb27d35cd0d744
unggu0704 <kyuhung1001@gmail.com>
2024-05-10 17:55:35 +0900

    Feat : Attraction [city] 가져오기

--- src/api/board.js
+++ src/api/board.js
@@ -2,9 +2,10 @@ import { localAxios } from "@/util/http-commons";
 
 const local = localAxios();
 
-function cityInfo(param, success, fail) {
-  local.get(`/attraction`, { params: param }).then(success).catch(fail);
+function cityInfo(success, fail) {
+  local.get(`/getCity`).then(success).catch(fail);
 }
+
 
 export {
   cityInfo,
---
+++ src/components/AttractionMain.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+      <!-- 하위 컴포넌트에 지역과 카테고리 정보 전달 -->
+
+      <!-- 검색 버튼 -->
+      <button @click="searchResults">검색</button>
+      <!-- 검색 결과 -->
+    </div>
+</template>
+  
+<script setup>
+  import { ref, onMounted } from 'vue';
+  import { cityInfo } from '@/api/board';
+ // import SearchResults from './SearchResults.vue'; // 하위 컴포넌트
+
+ onMounted(() => {
+  getArticleInfo();
+ });
+
+  const categories = ref([/* 초기 카테고리 데이터 */]);
+  const selectedRegions = ref([]);
+  const selectedCategories = ref([]);
+  const results = ref([]);
+
+
+
+const cities = ref([/* 초기 지역 데이터 */]);
+const citycodes = ref([/* 초기 지역 데이터 */]);
+const getArticleInfo = () => {
+  cities
+  cityInfo(
+    ({ data }) => {
+      console.log("response 객체 : " + data)
+      cities.value = data;
+      console.log("서버에서 정보목록 얻어오자1!!!", cities.value);
+    },
+    (error) => {
+      console.log("error 발생! " + error);
+    }
+    );
+};
+
+</script>
+  
