287da238a173962928e471c0eed3bd152a03963f
unggu0704 <kyuhung1001@gmail.com>
2024-05-17 13:51:52 +0900

    Feat : 라우터 설정 및 전역 guard 설정

--- src/router/index.js
+++ src/router/index.js
@@ -4,8 +4,28 @@ import LoginView from '@/views/LoginView.vue';
 import MainPageView from '@/views/MainPageView.vue';
 import MyMapView from '@/views/MyMapView.vue';
 import MyPage from '@/views/MyPage.vue';
-
+import { storeToRefs } from "pinia";
 import { createRouter, createWebHistory } from 'vue-router';
+
+import { useMemberStore } from "@/stores/member";
+const onlyAuthUser = async (to, from, next) => {
+  console.log("onlyAuthUser!!!")
+  const memberStore = useMemberStore();
+  const { userInfo, isValidToken } = storeToRefs(memberStore);
+  const { getUserInfo } = memberStore;
+
+  let token = sessionStorage.getItem("accessToken");
+
+  if (userInfo.value != null && token) {
+    await getUserInfo(token);
+  }
+  if (!isValidToken.value || userInfo.value === null) {
+    next({ name: "user-login" });
+  } else {
+    next();
+  }
+};
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -36,7 +56,38 @@ const router = createRouter({
       path: '/mypage',
       name: 'mypage',
       component: MyPage
-    }
+    },
+    {
+      path: "/board",
+      name: "board",
+      component: () => import("@/views/TheBoardView.vue"),
+      redirect: { name: "article-list" },
+      children: [
+        {
+          path: "list",
+          name: "article-list",
+          component: () => import("@/components/boards/BoardList.vue"),
+        },
+        {
+          path: "view/:articleno",
+          name: "article-view",
+          beforeEnter: onlyAuthUser,
+          component: () => import("@/components/boards/BoardDetail.vue"),
+        },
+        {
+          path: "write",
+          name: "article-write",
+          beforeEnter: onlyAuthUser,
+          component: () => import("@/components/boards/BoardWrite.vue"),
+        },
+        {
+          path: "modify/:articleno",
+          name: "article-modify",
+          beforeEnter: onlyAuthUser,
+          component: () => import("@/components/boards/BoardModify.vue"),
+        },
+      ],
+    },
 
   ]
 })
