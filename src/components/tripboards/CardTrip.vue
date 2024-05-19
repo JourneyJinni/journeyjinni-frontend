ba41ee5f64a3293ea4cf038a64e167144f22ee34
UNNGU0704 <kyuhung1001@gmail.com>
2024-05-19 20:50:17 +0900

    Fix : 라우터 이름 변경

--- src/components/tripboards/CardTrip.vue
+++ src/components/tripboards/CardTrip.vue
@@ -4,7 +4,7 @@
     <div class="card-body">
       <h5 class="card-title">{{ post.title }}</h5>
       <p class="card-text">{{ post.content }}</p>
-      <router-link :to="{ name: 'TripDetail', params: { id: post.id }}" class="btn btn-primary">자세히 보기</router-link>
+      <router-link :to="{ name: 'tripdetail', params: { id: post.id }}" class="btn btn-primary">자세히 보기</router-link>
     </div>
   </div>
 </template>
