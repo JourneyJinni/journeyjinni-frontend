4554e9e32478bce4da71449567d08d7604576389
UNNGU0704 <kyuhung1001@gmail.com>
2024-05-19 20:50:34 +0900

    Feat : 여행 코스 자세히 보기 TripDetail.vue 스켈레톤 코드

---
+++ src/components/tripboards/TripDetail.vue
@@ -0,0 +1,14 @@
+<template>
+
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  post: {
+    type: Object,
+    required: true
+  }
+});
+</script>
