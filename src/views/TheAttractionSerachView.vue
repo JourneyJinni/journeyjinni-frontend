1c2ad3b7b661d9a9b619e907d2bdf524413eb10c
unggu0704 <kyuhung1001@gmail.com>
2024-05-20 17:48:24 +0900

    Feat : Attraction 상위 View 구현

---
+++ src/views/TheAttractionSerachView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container mt-3">
+    <div class="row">
+      <div class="col-md-12">
+        <AttractionByCityAndCategory />
+      </div>
+
+      <div class="col-md-12">
+        <AttractionListItem/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, watch } from 'vue';
+import AttractionByCityAndCategory from "@/components/attraction/AttractionByCityAndCategory.vue";
+import { useAttractionStore } from "@/stores/Attraction.js";
+import AttractionListItem from "@/components/attraction/AttractionListItem.vue";
+
+const { searchedList } = useAttractionStore();
+
+watch(searchedList, () => {
+});
+</script>
+
+<style scoped>
+</style>
