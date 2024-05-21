db4ccaaa2080f9e70e3317d27a7b4fbc61545791
unggu0704 <kyuhung1001@gmail.com>
2024-05-21 11:11:18 +0900

    Feat : 이미지 업로드 및 메타데이터 코드 작성

--- src/components/attraction/AttractionByImageLocation.vue
+++ src/components/attraction/AttractionByImageLocation.vue
@@ -1,8 +1,51 @@
 <template>
-이미지 기반
+  <div class="mb-3">
+    <label for="destinationImages" class="form-label">이미지 업로드</label>
+    <input class="form-control" type="file" id="destinationImages" accept="image/*" @change="getMetadata">
+  </div>
+
 </template>
 
-<script>
+<script setup>
+import exifr from 'exifr';
+import {ref} from "vue";
+import {useAttractionStore} from "@/stores/Attraction.js";
+import {getLocationList} from "@/api/attraction.js";
+
+const attractionStore = useAttractionStore();
+
+const coordinate = ref({
+  latitude: "23",
+  longitude: "32"
+});
+
+
+// 메타데이터를 추출하는 함수
+const getMetadata = async (event) => {
+  const files = event.target.files;
+  try {
+    console.log("file", files);
+    let metadata =''
+    for (const file of files) {
+      metadata = await exifr.parse(file);
+    }
+    console.log("metadata", metadata);
+    coordinate.value.latitude = metadata.latitude;
+    coordinate.value.longitude = metadata.longitude;
+
+    getLocationList(
+        coordinate.value,
+        (response) => {
+          attractionStore.searchedList = [];
+          attractionStore.searchedList = response.data;
+        }
+    )
+  } catch (error) {
+    console.error('메타데이터가 없습니다...', error);
+    return {};
+  }
+};
+
 
 </script>
 
