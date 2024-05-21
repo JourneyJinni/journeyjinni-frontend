e29a2576060e5600738809015ba6e79e99e963b3
unggu0704 <kyuhung1001@gmail.com>
2024-05-21 17:20:17 +0900

    Style : Main Page 스타일 변경

--- src/components/main/LocalAttracionList.vue
+++ src/components/main/LocalAttracionList.vue
@@ -1,35 +1,35 @@
-<template >
+<template>
 
-    <div class="container-fluid" id="main1_div">
-      <div class="row align-items-center position-relative text-center">
-        <h2 class="text-secondary fw-bolder mt-1">여기는 어떠세요?</h2>
-        <h4>우리나라 곳곳의 명소입니다.</h4>
-        <div class="card-footer">
-          <router-link :to="{name : 'attraction'}" class="btn btn-primary stretched-link">더 많은 관광지 검색</router-link>
-        </div>
+  <div class="container-fluid" id="main1_div">
+    <div class="row align-items-center position-relative text-center">
+      <h2 class="text-secondary fw-bolder mt-1">여기는 어떠세요?</h2>
+      <h4>우리나라 곳곳의 명소입니다.</h4>
+      <div class="card-footer">
+        <router-link :to="{name : 'attraction'}" class="btn btn-primary stretched-link">더 많은 관광지 검색</router-link>
       </div>
     </div>
+  </div>
   <!-- 각 카드 실행 -->
   <div class="container-fluid">
     <div class="col-md-12 mt-3">
       <div class="row fw-bold text-center">
       </div>
-        <div class="row mt-3">
-      <div class="card col-lg-3 col-md-6" v-for = "(attraction,index) in mainAttractionList" :key="attraction.id">
-      <div class="card-img-wrapper mt-2">
-        <img :src="attraction.firstImage" class="card-img-top img-fluid" alt="" 
-        onerror="this.onerror=null; this.src='src/assets/noimg.jpg'" >
-      </div>
-      <div class="card-body">
-          <h5 class="card-title">{{attraction.title}}</h5>
-          <p class="card-text">{{attraction.addr1 + attraction.addr2}}</p>
-          
-          <!-- 고유한 모달 ID를 data-target에 설정 -->
-          <button type="button" class="btn btn-primary btn-sm" @click="showModal[index] = true">
+      <div class="row mt-3">
+        <div class="card col-lg-3 col-md-6" v-for="(attraction,index) in mainAttractionList" :key="attraction.id">
+          <div class="card-img-wrapper mt-2">
+            <img :src="attraction.firstImage" class="card-img-top img-fluid" alt=""
+                 onerror="this.onerror=null; this.src='src/assets/noimg.jpg'">
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">{{ attraction.title }}</h5>
+            <p class="card-text">{{ attraction.addr1 + attraction.addr2 }}</p>
+
+            <!-- 고유한 모달 ID를 data-target에 설정 -->
+            <button type="button" class="btn btn-primary btn-sm" @click="showModal[index] = true">
               더보기
             </button>
             <!-- <AttractionModal :attraction="attraction" :show-modal="showModal[index]" @close-modal="closeModal(index)" /> -->
-        </div>
+          </div>
         </div>
       </div>
     </div>
@@ -40,18 +40,18 @@
     
     <AttractionModal :show-modal='showModal'/> -->
 
-</div>
+  </div>
 </template>
 
 <script setup>
 import AttractionModal from '@/components/AttractionModal.vue';
 import axios from "axios";
-import { ref } from "vue";
+import {ref} from "vue";
 
 const showModal = ref(false);
 const showModalMthod = () => {
   showModal.value = true;
-  console.log("click" , showModal.value);
+  console.log("click", showModal.value);
 }
 
 const closeModal = () => {
@@ -60,16 +60,16 @@ const closeModal = () => {
 const mainAttractionList = ref([]);
 
 axios.get("http://localhost/")
-  .then(({ data }) => {
-    mainAttractionList.value = data;
-    console.log(mainAttractionList)
-  })
-  .catch((error) => {
-    alert(error)
-  })
+    .then(({data}) => {
+      mainAttractionList.value = data;
+      console.log(mainAttractionList)
+    })
+    .catch((error) => {
+      alert(error)
+    })
 </script>
 <style scope>
-.box{
+.box {
   width: 100vw;
   height: 100vh;
 }
--- src/components/main/MainCardTrip.vue
+++ src/components/main/MainCardTrip.vue
@@ -1,19 +1,30 @@
 <script setup>
 
 import {onMounted, ref} from "vue";
-import {getTrip} from "@/api/Trip.js";
+import {getTrip, getTripImages} from "@/api/Trip.js";
+import CardTrip from "@/components/tripboards/CardTrip.vue";
 
 onMounted(() => {
   trips();
 });
 
 const posts = ref([]);
-
+const images = ref([]);
 const trips = async () => {
   console.log("trip 정보를 가지러 감")
   getTrip(
       (success) => {
         posts.value = success.data; // 값에 직접 접근
+        posts.value.forEach(post => {
+          getTripImages(
+              post.tripId,
+              (response) => {
+                post.images = response.data;
+              }, (error) => {
+              }
+          )
+        });
+
         console.log(posts.value)
       },(error) => {
         console.log("여행 코스들을 가져오는데 실패했습니다.")
@@ -21,32 +32,29 @@ const trips = async () => {
   );
 };
 
+
 </script>
 
 <template>
   <div class="container">
-    <div class="row row align-items-center position-relative text-center">
-    <h2 class="text-secondary fw-bolder mt-2">다른 사람들은 이런 여행지를 갔어요.</h2>
+    <div class="container-fluid" id="main1_div">
+      <div class="row row align-items-center position-relative text-center">
+      <h2 class="text-secondary fw-bolder mt-1">다른 사람들은 이런 여행지를 갔어요.</h2>
+      <br><br>
       <div class="card-footer">
         <router-link :to="{name : 'my-map'}" class="btn btn-primary stretched-link">나의 여행지도 만들러가기</router-link>
       </div>
       <br><br><br>
-    <div class="row">
-      <div class="col-md-12">
-        <div class="card-deck d-flex flex-nowrap overflow-auto">
-          <div class="card mb-3 me-3" v-for="(post, tripId) in posts.slice(0, 5)" :key="tripId">
-            <img :src="post.image || 'resources\\images.jpg'" class="card-img-top" alt="Trip Image">
-            <div class="card-body">
-              <h5 class="card-title">{{ post.tripName }}</h5>
-              <p class="card-text">{{ post.description }}</p>
-            </div>
-            <div class="card-footer">
-              <router-link :to="{ name: 'tripdetail', params: { tripId: post.tripId }}" class="btn btn-primary stretched-link">자세히 보기</router-link>
+      </div>
+<!--      <div class="row">-->
+<!--        <div class="col-md-12">-->
+          <div class="card-deck d-flex flex-nowrap overflow-auto">
+            <div v-for="(post, tripId) in posts.slice(0, 5)" :key="tripId" style="width: 18rem;">
+              <CardTrip :trip="post" />
             </div>
           </div>
-        </div>
-      </div>
-    </div>
+<!--        </div>-->
+<!--      </div>-->
     </div>
   </div>
 
