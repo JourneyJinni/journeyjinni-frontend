357d422989006c2c25eae04c2a3f81f278bb168f
unggu0704 <kyuhung1001@gmail.com>
2024-05-22 17:45:39 +0900

    Feat : opencv 스켈레톤

--- src/components/attraction/AttractionByImageLocation.vue
+++ src/components/attraction/AttractionByImageLocation.vue
@@ -13,9 +13,12 @@
 
   <div class="mb-3">
     <label for="destinationImages" class="form-label fs-5 fw-bold">이미지 업로드</label>
-    <input class="form-control" type="file" id="destinationImages" accept="image/*" @change="getMetadata">
+    <input class="form-control" type="file" id="destinationImages" accept="image/*" @change="handleImageUpload">
   </div>
-
+  <div class="modal-footer">
+    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">취소</button>
+    <button type="button" class="btn btn-primary" data-bs-dismiss="modal" @click="submitForm">확인</button>
+  </div>
 </template>
 
 <script setup>
@@ -23,6 +26,8 @@ import exifr from 'exifr';
 import {ref} from "vue";
 import {useAttractionStore} from "@/stores/Attraction.js";
 import {getLocationList} from "@/api/attraction.js";
+import imageCompression from "browser-image-compression";
+import axios from "axios";
 
 const attractionStore = useAttractionStore();
 
@@ -30,6 +35,44 @@ const coordinate = ref({
   latitude: "23",
   longitude: "32"
 });
+
+const options = {
+  maxSizeMB: 1, // 허용하는 최대 사이즈 지정
+  maxWidthOrHeight: 1920, // 허용하는 최대 width, height 값 지정
+  useWebWorker: true // webworker 사용 여부
+};
+
+const image = ref([]);
+const handleImageUpload = async (event) => {
+  const files = event.target.files;
+  for (const file of files) {
+    // 메타데이터 추출
+    await getMetadata(file);
+
+    console.log("file : ", file);
+    // 이미지 압축
+    const compressedFile = await imageCompression(file, options);
+
+    image.value.push(compressedFile);
+    //console.log(++count);
+  }
+};
+
+const submitForm = async () => {
+  const formData = new FormData();
+  console.log("image : " , image.value[0]);
+  formData.append('images', image.value[0], `image1.jpg`);
+  try {
+    const response = await axios.post('http://localhost/compare', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    });
+    console.log(response.data);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 
 // 메타데이터를 추출하는 함수
