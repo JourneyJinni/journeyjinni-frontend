cdad9535a027f3e254cadd9e39c6d1d3e6eb7747
UNGGU0704 <130115689+UNGGU0704@users.noreply.github.com>
2024-08-28 22:37:02 +0900

    Chore : vite의 localhost 외부 접속 허용 (#13)
    - 기본적으로 vite는 localhost의 외부 접속이 불가능
    - 이를 해결하기 위해 `0.0.0.0` 을 server 설정에 추가

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 import VueDevTools from 'vite-plugin-vue-devtools'
-
 // https://vitejs.dev/config/
+
+
 export default defineConfig({
   plugins: [
     vue(),
@@ -16,6 +17,7 @@ export default defineConfig({
     }
   },
   server: {
+    host: '0.0.0.0', // 모든 IP에서 접근 가능하게 함
     proxy: {
       '/api': {
         target: 'http://localhost', // 백엔드 서버 주소
