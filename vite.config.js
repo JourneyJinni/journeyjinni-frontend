299d41fcf80e7d8f72a72fd41247ca3d529d8a56
UNNGU0704 <kyuhung1001@gmail.com>
2025-02-12 02:39:14 +0900

    Update: vite.config.js 원복

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 import VueDevTools from 'vite-plugin-vue-devtools'
-const { VITE_VUE_API_URL } = import.meta.env;
 // https://vitejs.dev/config/
 
 
