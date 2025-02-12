9c79993c9e72c1b09678c0cfe4733f896ffa9193
UNGGU0704 <130115689+UNGGU0704@users.noreply.github.com>
2025-02-12 20:23:13 +0900

    Update: Backend Enpoint 주소 External-IP로 변경

--- src/util/http-commons.js
+++ src/util/http-commons.js
@@ -4,13 +4,12 @@ const { VITE_VUE_API_URL } = import.meta.env;
 
 // local vue api axios instance
 function localAxios() {
-  const instance = axios.create({
+  return axios.create({
     baseURL: VITE_VUE_API_URL,
     headers: {
-      "Content-Type": "application/json;charset=utf-8",
+      "Content-Type": "application/json"
     },
   });
-  return instance;
 }
 
 export { localAxios };
