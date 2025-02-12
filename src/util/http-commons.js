ff2eb0591699f586f52483524c4abfddbb45ac3d
UNGGU0704 <130115689+UNGGU0704@users.noreply.github.com>
2025-02-12 20:41:37 +0900

    Revert "Update: Backend Enpoint 주소 External-IP로 변경"
    This reverts commit 9c79993c9e72c1b09678c0cfe4733f896ffa9193.

--- src/util/http-commons.js
+++ src/util/http-commons.js
@@ -4,12 +4,13 @@ const { VITE_VUE_API_URL } = import.meta.env;
 
 // local vue api axios instance
 function localAxios() {
-  return axios.create({
+  const instance = axios.create({
     baseURL: VITE_VUE_API_URL,
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json;charset=utf-8",
     },
   });
+  return instance;
 }
 
 export { localAxios };
